ac5b21d7a44a69da0b6b5a7d0155e4a3e34f6b44
svehbak-lab <svehbak@gmail.com>
2026-05-23 18:46:16 +0000

    Dashboard fixes: fullset pricing, card count, set rules

--- MtgoBot.Dashboard/obj/Debug/net9.0/MtgoBot.Dashboard.AssemblyInfo.cs
+++ MtgoBot.Dashboard/obj/Debug/net9.0/MtgoBot.Dashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MtgoBot.Dashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e4c9d9760406a8878dc1472404be62ff280ba06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5806c8a82b0c5eb0ec9bb8ef82c592c03ce817d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MtgoBot.Dashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MtgoBot.Dashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
